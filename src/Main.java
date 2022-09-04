72447c76261bc3f3bc73466b87f351255baf6917
AndreyChirkin <106620486+AndrewChirkin@users.noreply.github.com>
2022-09-04 20:24:56 +0300

    hw3pt2ex5

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        int deliveryDistance = 95;
-        double daysDelivery;
-        int distancePenality = 40;
-        daysDelivery = (deliveryDistance - 61) / distancePenality + 3;
-        double result = Math.ceil(daysDelivery);
-        if(deliveryDistance <= 20) {
-            System.out.println("Потребуется один день доставки!");
-        } else if(deliveryDistance < 61 && deliveryDistance > 20){
-            System.out.println("Потребуется 2 дня доставки!");
-        } else {
-            System.out.println("Потребуется " + daysDelivery + " дней на доставку.");
+        int monthNumber = 1;
+
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Время года - зима.");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Время года - весна.");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Время года - лето.");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Время года - осень.");
+                break;
+            default:
+                System.out.println("Такого месяца не существует!");
         }
     }
         }
