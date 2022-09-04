c9bf11156280f9aed2af939e59c6b9001c534d1a
AndreyChirkin <106620486+AndrewChirkin@users.noreply.github.com>
2022-09-04 18:49:22 +0300

    hw3pt2ex3

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int iOS = 0;
-        int productionYear = 2001;
-        int Android = 1;
-        int clientOS = 1;
-        if(productionYear >= 2015) {
-            if (clientOS == Android) {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            }
-        }else
-            if (clientOS == Android) {
-                System.out.println("Установите облегчённую версию приложения для Android по ссылке");
-            }else {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            }
-}
+        int year = 2000;
+        if (year% 4 == 0 && year% 100 != 0 || year% 400 == 0) {
+            System.out.println("Год вискосный!");
+        }else System.out.println("Год невисокосный!");
+    }
         }
 
 
