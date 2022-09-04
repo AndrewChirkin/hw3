a22eb8b3d550530402ce2c2fb77db1e388e52ae5
AndreyChirkin <106620486+AndrewChirkin@users.noreply.github.com>
2022-09-04 14:24:01 +0300

    hw3pt1ex2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 666;
-        if (age >= 18) {
-            System.out.println("Поздравляю с совершеннолетием!");
+        int age = 100;
+        if (age >= 7 && age < 18) {
+            System.out.println("Возраст, когда ребёнку пора идти в школу.");
         }
-        if (age < 18) {
-            System.out.println("Возраст совершеннолетия не наступил, нужно подождать!");
+        if (age >= 18 && age < 24) {
+            System.out.println("Человек уже закончил школу и может отправляться в университет");
+        }
+        if (age >= 24) {
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
         }
 
 
