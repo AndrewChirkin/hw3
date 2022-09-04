c4f8fafaaa81cebe61ac1aef5b3165ed1cc3913a
AndreyChirkin <106620486+AndrewChirkin@users.noreply.github.com>
2022-09-04 15:58:30 +0300

    hw3pt1ex6

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,17 @@ public class Main {
         int tree = 1;
         if (one > two && one > tree) {
             System.out.println("Число " + one + " - самое большое!");
-            if (tree > two) {
-                System.out.println("Число " + tree + " - самое большое!");
+        }
+            else {
+                if (two > tree){
+                    System.out.println("Число " + two + " - самое большое!");
+
+                }else {
+                    System.out.println("Число " + tree + " - самое большое!");
+                }
             }
-        } else if (tree > two) {
-            System.out.println("Число " + tree + " - самое большое!");
 
-        } else if(two >tree)
 
-    {
-        System.out.println("Число " + two + " - самое большое!");
-    }
 }
         }
 
