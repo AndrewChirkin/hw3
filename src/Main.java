6df1bca9da6ee6f62278d6ff874f777dac61edb6
AndreyChirkin <106620486+AndrewChirkin@users.noreply.github.com>
2022-09-04 17:55:06 +0300

    hw3pt2ex2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         int iOS = 0;
+        int productionYear = 2001;
         int Android = 1;
-        int clientOS = 0;
-        if(clientOS == iOS){
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        }else {
-            System.out.println("Установите версию приложения для Android по ссылке");
-
-        }
-
+        int clientOS = 1;
+        if(productionYear >= 2015) {
+            if (clientOS == Android) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        }else
+            if (clientOS == Android) {
+                System.out.println("Установите облегчённую версию приложения для Android по ссылке");
+            }else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
 }
         }
 
