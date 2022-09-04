666f174bc109c61441d1a18f60130626094a532a
AndreyChirkin <106620486+AndrewChirkin@users.noreply.github.com>
2022-09-04 15:00:10 +0300

    hw3pt1ex5

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int age = 100;
-        if (age <= 6 && age >= 2) {
-            System.out.println("Если возраст человека равен " + age + " лет, то ему нужно ходить в детский сад.");
+        if (age < 5) {
+            System.out.println("Возраст слишком мал, кататься нельзя.");
         }
-        if (age >= 7 && age <= 18) {
-            System.out.println("Если возраст человека равен " + age + " лет, то ему нужно ходить в школу.");
+        if (age >= 5 && age <= 14) {
+            System.out.println("Данный ребёнок может кататься только в сопровождении взрослого.");
         }
-        if (age > 18 && age <= 24) {
-            System.out.println("Если возраст человека равен " + age + " лет, то ему нужно ходить в университет");
-        }
-        if (age > 24) {
-            System.out.println("Если возраст человека равен " + age + " лет, то ему пора ходить на работу.");
+        if (age > 14) {
+            System.out.println("Можно кататься без ограничений.");
         }
 
 
