ecea62bf60478f2cc1e2a5c04c045f6b45fb9ae2
AndreyChirkin <106620486+AndrewChirkin@users.noreply.github.com>
2022-09-04 14:03:59 +0300

    hw3pt1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 666;
+        if (age >= 18) {
+            System.out.println("Поздравляю с совершеннолетием!");
+        }
+        if (age < 18) {
+            System.out.println("Возраст совершеннолетия не наступил, нужно подождать!");
+        }
+
+
+
+
+
+        System.out.println();
+    }
+}
